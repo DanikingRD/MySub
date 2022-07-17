cfb361a5099d8ad485736a3110639a6e4ab7816b
DanikingRD <danikingrd@gmail.com>
2022-07-17 11:37:11 -0400

    Add firebase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
-
-
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mysub/firebase_options.dart';
 import 'package:mysub/mysub.dart';
 
-void main(List<String> args) {
-  runApp(
-    const MySubApp()
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const MySubApp());
 }
 
